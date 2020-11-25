e8b34f84df53aa6b483484ee3fef34d7d6e47343
KFkas <48663613+JaoKFkas@users.noreply.github.com>
2020-11-25 15:10:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ if(type == "Windows"):
 else:
     sys.exit("\nSorry, our program don't have support for your system\n")
 
+# Version
+
+version="0.0.2"
+
 # Program
 
 import pygame, math, subprocess, requests, os, shutil
@@ -17,46 +21,91 @@ from pygame.locals import *
 pygame.init()
 
 display = pygame.display.set_mode((800, 600))
-pygame.display.set_caption('Menu')
+pygame.display.set_caption('KFkas Launcher')
 
 clock = pygame.time.Clock()
 
 loading = False
 
+exists = os.path.exists
+
 # Path to Sprites
 
-path = './System/'
+pathS = './System/Sprites/'
+
+# Path to Sounds
+
+pathM = './System/Sounds/'
 
 # Background Variables
 
 hue = 0
-bgSprite = pygame.image.load(path+'Background.png')
-bgSprite2 = bgSprite.copy()
+
+bgSprite = False
+
+if exists(pathS+'Background.png'):
+    bgSprite = pygame.image.load(pathS+'Background.png')
+
 bgX = 0
 bgXAngle = 0
+
 bgYStart = -200
 bgY = bgYStart
 bgYAngle = 0
 
 # Mouse Variable
 
-mouseSprite = pygame.image.load(path+'Mouse.png')
-print(mouseSprite)
+mouseSprite = False
+
+if exists(pathS+'Mouse.png'):
+    mouseSprite = pygame.image.load(pathS+'Mouse.png')
 
 mousePos = pygame.mouse.get_pos()
-pygame.mouse.set_visible(False)
+
+if mouseSprite != False:
+    pygame.mouse.set_visible(False)
 
 # Cloud Variable
 
-cloudSprite = pygame.image.load(path+'Cloud.png')
-cloudWidth, cloudHeight = cloudSprite.get_rect().size
-cloudX, cloudY = (128, 128)
+cloudSprite = False
+
+if exists(pathS+'Cloud.png'):
+    cloudSprite = pygame.image.load(pathS+'Cloud.png')
+    cloudWidth, cloudHeight = cloudSprite.get_rect().size
+
+cloudX, cloudYStart = (128, 256)
+cloudY = cloudYStart
 
 # Update Variable
 
-updateSprite = pygame.image.load(path+'Update.png')
-updateWidth, updateHeight = updateSprite.get_rect().size
-updateX, updateY = (256, 128)
+updateSprite = False
+
+if exists(pathS+'Exit.png'):
+    updateSprite = pygame.image.load(pathS+'Update.png')
+    updateWidth, updateHeight = updateSprite.get_rect().size
+
+updateX, updateYStart = (256, 256)
+updateY = updateYStart
+
+# Exit Variable
+
+exitSprite = False
+
+if exists(pathS+'Exit.png'):
+    exitSprite = pygame.image.load(pathS+'Exit.png')
+
+exitWidth, exitHeight = exitSprite.get_rect().size
+exitX, exitY = (700, 500)
+exitAlpha = 0
+
+# Version Variable
+
+versionSprite = False
+
+if exists(pathS+'Version.png'):
+    versionSprite = pygame.image.load(pathS+'Version.png')
+
+versionPos = (30, 500)
 
 # Defs
 
@@ -69,14 +118,21 @@ def getCollision(pos1, pos2, size):
     else:
         return False
 
+# Create Event
+
+close = False
+
+#pygame.mixer.music.load(pathM+'Hotline.mp3')
+#pygame.mixer.music.play(loops=1, start=0.0, fade_ms = 10000)
+
 while True:
 
     # Close Game
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            loading = True
+            close = True
 
     clock.tick(60)
 
@@ -85,8 +141,13 @@ while True:
     mousePos = pygame.mouse.get_pos()
 
     keys = pygame.key.get_pressed()
+    mouses = pygame.mouse.get_pressed()
     
-    if keys[K_ESCAPE]:
+    if keys[K_ESCAPE] and not loading:
+        loading = True
+        close = True
+
+    if close and loading and bgYStart <= -200:
         pygame.quit()
         sys.exit()
 
@@ -103,20 +164,27 @@ while True:
 
     # Draw Cloud
 
-    #if getCollision(mousePos, (cloudX+cloudWidth/2, cloudY+cloudHeight/2), 32):
-    display.blit(cloudSprite, (cloudX, cloudY))
+    if getCollision(mousePos, (cloudX+cloudWidth/2, cloudY+cloudHeight/2), 32) and mouses[0]:
+        subprocess.run("python ./Cloud/fileSynch.py")
+
+    cloudY = cloudYStart - bgYStart/5
+
+    if cloudSprite != False:
+        display.blit(cloudSprite, (cloudX, cloudY))
 
     # Draw Update
 
-    if getCollision(mousePos, (updateX+updateWidth/2, updateY+updateHeight/2), 32):
-        url = "https://raw.githubusercontent.com/JaoKFkas/kfkas_launcher/main/System/update.py"
-        r = requests.get(url)
-        open('update.py', 'wb').write(r.content)
+    #if getCollision(mousePos, (updateX+updateWidth/2, updateY+updateHeight/2), 32):
+    #    url = "https://raw.githubusercontent.com/JaoKFkas/kfkas_launcher/main/System/update.py"
+    #    r = requests.get(url)
+    #    open('update.py', 'wb').write(r.content)
 
-        subprocess.run("python update.py")
-        pygame.quit()
-        sys.exit()
-    
+    #    subprocess.run("python update.py")
+    #    pygame.quit()
+    #    sys.exit()
+
+    updateY = updateYStart - bgYStart/5
+
     display.blit(updateSprite, (updateX, updateY))
 
     # Draw Background
@@ -126,7 +194,7 @@ while True:
     if bgX > 800:
         bgX = 0
 
-    bgXAngle += 0.01
+    bgXAngle += 0.05
 
     if bgXAngle > 10:
         bgXAngle = -10
@@ -143,12 +211,38 @@ while True:
     bgY = bgYStart + math.sin(bgYAngle/100)*32
 
     pygame.draw.rect(display, (0, 30, 60), (0, bgY+150, 800, 700))
-    display.blit(pygame.transform.rotate(bgSprite, math.cos(bgXAngle)), (bgX, bgY))
-    display.blit(pygame.transform.rotate(bgSprite, math.cos(bgXAngle)), (bgX-800, bgY))
+
+    if bgSprite != False:
+        display.blit(pygame.transform.rotate(bgSprite, math.cos(bgXAngle)), (bgX, bgY))
+        display.blit(pygame.transform.rotate(bgSprite, math.cos(bgXAngle)), (bgX-800, bgY))
+
+    # Draw Exit
+
+    if exitAlpha >= 1 and exitSprite != False:
+        if getCollision(mousePos, (exitX+exitWidth/2, exitY+exitHeight/2), 32) and mouses[0]:
+            loading = True
+            close = True
+
+    if loading:
+        if exitAlpha >= 0:
+            exitAlpha-=0.05
+    else:
+        if bgYStart >= 300 and exitAlpha < 1:
+            exitAlpha+=0.01
+
+    if exitSprite != False:
+        exitSprite.set_alpha(exitAlpha*1000)
+        display.blit(exitSprite, (exitX, exitY))
+
+    # Draw Version
+
+    if versionSprite != False:
+        display.blit(versionSprite, versionPos)
 
     # Draw Mouse
 
-    display.blit(mouseSprite, mousePos)
+    if mouseSprite != False:
+        display.blit(mouseSprite, mousePos)
 
     print(clock.get_fps())
 
